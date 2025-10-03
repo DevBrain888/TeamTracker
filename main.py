066e3d8d6911fe0b698340820c3fe5e8e513635a
DevBrain888 <bababyibabijon@gmail.com>
2025-10-03 18:23:00 +0300

    Added all project

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import uvicorn
+from contextlib import asynccontextmanager
+from database import create_tables, delete_tables
+from router import router as tasks_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("Tables deleted")
+    await create_tables()
+    print("Tables created")
+    yield
+    print("Shutdown...")
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(tasks_router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)  # Добавил host/port для ясности
